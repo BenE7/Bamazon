9a3e263ec6518cff9d4370ef297b8cf5eb7d5e16
Ben Evans <benevans@Bens-MacBook-Pro.local>
2018-02-03 09:52:00 -0500

    firstpartend

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var masterID = 0;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,12 +16,17 @@ var connection = mysql.createConnection({
 
 function Purchase (ID, quantity){
     this.ID = ID
+    masterID = ID;
     this.quantity = quantity
 };
 
 function checkQuantity(answers, item){
     if(answers.Quantity <= item.Stock_Quantity){
+        var newTotal = (item.Stock_Quantity - answers.Quantity);
+        
+        //console.log(newTotal);
         console.log("You're in luck! Your product is in stock!")
+        alterTable(newTotal, masterID)
     }else if(answers.Quantity > item.Stock_Quantity){
         console.log("Sorry, our stock can not meet your needs.")
     }
@@ -32,6 +38,15 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
     afterConnection();
 });
+
+function alterTable(answers, id){
+    //connection.query("UPDATE Products SET Stock_Quantity = " + answers + "WHERE Item_ID = " + id, function (err, res) {
+    //if (err) throw err;
+    //console.log(res);
+    console.log(" You now have " + answers + " in Stock!");
+    }
+    //connection.query("ALTER TABLE Products")
+   // )};
 
 function afterConnection() {
     connection.query("SELECT * FROM Bamazon.Products", function (err, res) {
@@ -63,10 +78,12 @@ function afterConnection() {
                 answers.Product,
                 answers.Quantity
                 
-            )
+                
+            );
             
             console.log(newPurchase);
         });
         connection.end();
     });
+    
 }
